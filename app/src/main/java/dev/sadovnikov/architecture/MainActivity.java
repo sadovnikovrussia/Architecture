8cfa5bfbe8575229a60ad5c3ffa426f6cca9b01c
Alexey <sadovnikov@st-hld.ru>
2018-12-12 19:41:24 +0300

    javaaaaaaaaaaaa

--- app/src/main/java/dev/sadovnikov/architecture/MainActivity.java
+++ app/src/main/java/dev/sadovnikov/architecture/MainActivity.java
@@ -7,17 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
+import io.reactivex.internal.disposables.DisposableContainer;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -32,55 +36,55 @@ public class MainActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.editText);
 
-        Observable<Integer> observable = Observable.just(1, 2, 4, 8);
-        observable.subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                Log.d(TAG, "onNext: " + integer + ", " + getLocalClassName());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.w(TAG, "onError: ", e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: ");
-            }
-        });
-
-        List<Integer> integers = new ArrayList<>();
-        for (Integer j = 0; j < 10; j++) {
-            integers.add(j);
-        }
-        List<Integer> integers2 = new ArrayList<>();
-        for (Integer j = 200; j < 300; j++) {
-            integers.add(j);
-        }
-
-        List<Integer> integers3 = new ArrayList<>();
-        for (Integer j = 500; j < 600; j++) {
-            integers3.add(j);
-        }
-        List<String> strings = new ArrayList<>();
-        strings.add("q");
-        strings.add("w");
-        strings.add("e");
-        strings.add("r");
-        strings.add("t");
-        strings.add("y");
-
-
-        Observable<Integer> observable2 = Observable.fromIterable(integers2);
-        Observable<Integer> observable3 = Observable.fromIterable(integers3);
-        Observable<Integer> observable1 = Observable.fromIterable(integers);
-        Observable<String> observable4 = Observable.fromIterable(strings);
+//        Observable<Integer> observable = Observable.just(1, 2, 4, 8);
+//        observable.subscribe(new Observer<Integer>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d(TAG, "onSubscribe: ");
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//                Log.d(TAG, "onNext: " + integer + ", " + getLocalClassName());
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.w(TAG, "onError: ", e);
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d(TAG, "onComplete: ");
+//            }
+//        });
+//
+//        List<Integer> integers = new ArrayList<>();
+//        for (Integer j = 0; j < 10; j++) {
+//            integers.add(j);
+//        }
+//        List<Integer> integers2 = new ArrayList<>();
+//        for (Integer j = 200; j < 300; j++) {
+//            integers.add(j);
+//        }
+//
+//        List<Integer> integers3 = new ArrayList<>();
+//        for (Integer j = 500; j < 600; j++) {
+//            integers3.add(j);
+//        }
+//        List<String> strings = new ArrayList<>();
+//        strings.add("q");
+//        strings.add("w");
+//        strings.add("e");
+//        strings.add("r");
+//        strings.add("t");
+//        strings.add("y");
+//
+//
+//        Observable<Integer> observable2 = Observable.fromIterable(integers2);
+//        Observable<Integer> observable3 = Observable.fromIterable(integers3);
+//        Observable<Integer> observable1 = Observable.fromIterable(integers);
+//        Observable<String> observable4 = Observable.fromIterable(strings);
 //        observable1
 //                .filter(integer -> integer > 10)
 //                .map(integer -> integer * 2)
@@ -95,10 +99,80 @@ public class MainActivity extends AppCompatActivity {
 //                .subscribe(
 //                        str -> Log.d(TAG, str.toString()),
 //                        e -> Log.w(TAG, e));
-        Observable
-                .zip(observable1, observable4, Bukvoed::kaka)
-                .subscribe(s -> Log.d(TAG, "onCreate: " + s));
+//        Observable
+//                .zip(observable1, observable4, Bukvoed::kaka)
+//                .subscribe(s -> Log.d(TAG, "onCreate: " + s));
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("http://publicobject.com/helloworld.txt").build();
+
+        Log.d(TAG, "onCreate: " + Thread.currentThread().getName());
+        Observable<Response> responseObservable = Observable.fromCallable(() -> client.newCall(request).execute());
+        Disposable disposable = responseObservable
+                .map(response -> {
+                    Log.d(TAG, "onMap1" + ", " + Thread.currentThread().getName());
+                    return response.body() != null ? response.body().string() : "Пустота";
+                })
+                .flatMap(s -> Observable.fromArray(new String[]{s, s}))
+                .map(s -> {
+                            Log.d(TAG, "onMap2" + ", " + Thread.currentThread().getName());
+                            return s + s;
+                        }
+                )
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(s -> Log.i(TAG, "onNext: " + s));
+        CompositeDisposable compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(disposable);
+        compositeDisposable.dispose();
+
+
+//        Observable<Object> observable = Observable.create(emitter -> client.newCall(request).enqueue(
+//                new Callback() {
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//                        Log.w(TAG, "onFailure: " + Thread.currentThread().getName(), e);
+//                        emitter.onError(e);
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//                        Log.d(TAG, "onResponse: " + Thread.currentThread().getName());
+//                        Log.d(TAG, "onResponse: " + response);
+//                        emitter.onNext(response.body().string());
+//                        emitter.onComplete();
+//                    }
+//                }));
+//        observable
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(new Observer<Object>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//                        Log.d(TAG, "onSubscribe: " + Thread.currentThread().getName());
+//                    }
+//
+//                    @Override
+//                    public void onNext(Object o) {
+//                        Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+//                        Log.d(TAG, "onNext: " + o);
+//                        editText.setText(o.toString());
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.d(TAG, "onError: " + Thread.currentThread().getName());
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.d(TAG, "onComplete: " + Thread.currentThread().getName());
+//                    }
+//                });
+
+
+//        Observable<String> observable5 = Observable.fromCallable(callable);
+//        observable5.subscribe(s -> Log.d(TAG, "fromCallable: " + s));
 //        RxSQLite.get().querySingle(RequestTable.TABLE, where)
 //                .compose(RxSchedulers.async())
 //                .flatMap(request -> {
@@ -111,7 +185,8 @@ public class MainActivity extends AppCompatActivity {
 //                    return RxSQLite.get().querySingle(CityTable.TABLE).compose(RxSchedulers.async());
 //                })
 
-
+        Observable<String> observable = Observable.just("Hello");
+        Observable<String> observable1 = observable.map(String::toUpperCase);
     }
 
 
@@ -139,7 +214,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class AsyncTransformer<T> implements ObservableTransformer<T, String> {
+
+    class AsyncTransformer<T> implements ObservableTransformer<T, String>,
+            Child {
 
         @Override
         public ObservableSource<String> apply(Observable<T> upstream) {
@@ -148,5 +225,26 @@ public class MainActivity extends AppCompatActivity {
                     .subscribeOn(Schedulers.computation())
                     .observeOn(AndroidSchedulers.mainThread());
         }
+
+
+        @Override
+        public void method2() {
+
+        }
+
+        @Override
+        public void method1() {
+
+        }
     }
+
+    interface Parent {
+        void method1();
+    }
+
+    interface Child extends Parent {
+
+        void method2();
+    }
+
 }
